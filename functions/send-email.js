18471c15931eede8dae739ddfc61461a67c26570
Angelina Holmqvist <angelinaholmqvist@live.se>
2023-11-18 12:29:02 +0100

    added smtp protocoll

--- functions/send-email.js
+++ functions/send-email.js
@@ -3,7 +3,9 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const transporter = nodemailer.createTransport({
-  service: "hotmail",
+  host: "smtp-mail.outlook.com",
+  port: 587,
+  secure: false, // true for 465, false for other ports
   auth: {
     user: "angelinaholmqvist@live.se",
     pass: process.env.EMAIL_PASSWORD,
